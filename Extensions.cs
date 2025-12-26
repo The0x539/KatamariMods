a8db673b1f5cbd70f4aed79d2ac7733b3c6a9b76
The0x539 <the0x539@gmail.com>
2025-12-26 13:06:40 -0600

    Remove unnecessary commented extension method

--- Extensions.cs
+++ Extensions.cs
@@ -10,32 +10,4 @@ internal static class Extensions {
         }
         return ret;
     }
-
-    /*
-    public static IEnumerable<T> GetComponentsInDescendants<T>(this GameObject obj)
-    where T : Component {
-        var s = new Stack<GameObject>();
-        s.Push(obj);
-        return GetComponentsInDescendantsCore<T>(s, false);
-    }
-
-    private static IEnumerable<T> GetComponentsInDescendantsCore<T>(Stack<GameObject> stack, bool includeSelf)
-        where T : Component {
-        var includeComponents = includeSelf;
-
-        while (stack.Count > 0) {
-            var current = stack.Pop();
-            if (includeComponents) {
-                if (current.GetComponent<T>() is T component) {
-                    yield return component;
-                }
-            }
-
-            foreach (var child in current.GetChildren()) {
-                stack.Push(child);
-            }
-            includeComponents = true;
-        }
-    }
-    */
 }
