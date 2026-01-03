19261afb54250a1dc8507aeed89bac81c8716e6e
The0x539 <the0x539@gmail.com>
2026-01-03 02:54:29 -0600

    Delta-time (parts of) NPC AI

--- FramerateUncap/Plugin.cs
+++ FramerateUncap/Plugin.cs
@@ -2,9 +2,13 @@
 
 using HarmonyLib;
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
+
+using UnityEngine;
 
 namespace FramerateUncap;
 
@@ -42,6 +46,49 @@ public sealed class Plugin : BaseUnityPlugin {
         }
     }
 
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(AttachableProp), nameof(AttachableProp.UpdateMono))]
+    public static IEnumerable<CodeInstruction> DeltaTimePropUpdates(IEnumerable<CodeInstruction> instructions) {
+        var input = instructions.ToArray();
+
+        static FieldInfo field(string name, BindingFlags flags = BindingFlags.Public) => typeof(AttachableProp).GetField(name, flags | BindingFlags.Instance);
+
+        var checkSimpleWait = field(nameof(AttachableProp.checkSimpleWait));
+        var checkSimpleWait2 = field(nameof(AttachableProp.checkSimpleWait2));
+        var s16EscapeTimer = field(nameof(AttachableProp.s16EscapeTimer), BindingFlags.NonPublic);
+        var deltaTime = typeof(Time).GetProperty(nameof(Time.deltaTime)).GetGetMethod();
+
+        for (var i = 0; i < input.Length; i++) {
+            var instr = input[i];
+
+            if (instr.LoadsConstant(30)) {
+                var next = input[i + 1];
+                if (next.StoresField(checkSimpleWait) || next.StoresField(checkSimpleWait2)) {
+                    instr.operand = 1000;
+                }
+            } else if (instr.LoadsField(checkSimpleWait) || instr.LoadsField(checkSimpleWait2) || instr.LoadsField(s16EscapeTimer)) {
+                CodeInstruction next = input[i + 1], next2 = input[i + 2];
+
+                if (next.opcode == OpCodes.Ldc_I4_1 && next2.opcode == OpCodes.Sub) {
+                    yield return instr;
+
+                    // Before: this.$field -= 1;
+                    // After: this.$field -= (int)(Time.deltaTime * 1000f;
+                    // TODO: Track microsecond rounding error like in the native-code equivalent?
+                    // Getting the lifecycle timing right might be tricky.
+                    yield return new(OpCodes.Call, deltaTime);
+                    yield return new(OpCodes.Ldc_R4, 1000f);
+                    yield return new(OpCodes.Mul);
+                    yield return new(OpCodes.Conv_I4);
+                    i++; // That stuff replaces LDC.I4.1. (The SUB stays intact.)
+
+                    continue;
+                }
+            }
+
+            yield return instr;
+        }
+    }
 }
 
 internal static class KatamariFfi {
