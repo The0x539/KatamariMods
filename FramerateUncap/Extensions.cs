9fffa3d452b768a22bf45296f127ff500b3059ab
The0x539 <the0x539@gmail.com>
2026-01-07 01:53:24 -0600

    properly uncap the update rate in GameManager.Update

---
+++ FramerateUncap/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+
+using System.Reflection.Emit;
+
+namespace FramerateUncap;
+
+internal static class Extensions {
+    public static CodeMatcher RemoveMatching(this CodeMatcher m, params CodeMatch[] ms) {
+        m.MatchForward(false, ms);
+        m.ThrowIfInvalid("Could not find a matching pattern of instructions");
+        var labels = m.Instruction.ExtractLabels();
+        m.RemoveInstructions(ms.Length);
+        m.AddLabels(labels);
+        return m;
+    }
+
+    public static CodeMatcher AssertPos(this CodeMatcher m, int pos) {
+        return m.ThrowIfFalse($"Wrong position: expected {pos}, got {m.Pos}", m => m.Pos == pos);
+    }
+
+    public static int ConstInt(this CodeInstruction i) {
+        if (i.opcode == OpCodes.Ldc_I4_M1) return -1;
+        if (i.opcode == OpCodes.Ldc_I4_0) return 0;
+        if (i.opcode == OpCodes.Ldc_I4_1) return 1;
+        if (i.opcode == OpCodes.Ldc_I4_2) return 2;
+        if (i.opcode == OpCodes.Ldc_I4_3) return 3;
+        if (i.opcode == OpCodes.Ldc_I4_4) return 4;
+        if (i.opcode == OpCodes.Ldc_I4_5) return 5;
+        if (i.opcode == OpCodes.Ldc_I4_6) return 6;
+        if (i.opcode == OpCodes.Ldc_I4_7) return 7;
+        if (i.opcode == OpCodes.Ldc_I4_8) return 8;
+        if (i.opcode == OpCodes.Ldc_I4_S) return (sbyte)i.operand;
+        return (int)i.operand;
+    }
+}
--- FramerateUncap/Plugin.cs
+++ FramerateUncap/Plugin.cs
@@ -14,11 +14,8 @@ namespace FramerateUncap;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public sealed class Plugin : BaseUnityPlugin {
-    public const float TARGET_FPS = 180;
-
     public void Awake() {
         Harmony.CreateAndPatchAll(this.GetType());
-        Harmony.CreateAndPatchAll(typeof(TimerPatches));
         KatamariFfi.InstallHooks();
         Application.runInBackground = true;
     }
@@ -38,20 +35,162 @@ public sealed class Plugin : BaseUnityPlugin {
 
     [HarmonyTranspiler]
     [HarmonyPatch(typeof(GameManager), nameof(GameManager.Update))]
-    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sAreaChange))]
-    [HarmonyPatch(typeof(GlobalManager), nameof(GlobalManager.FadeUpdate))]
-    [HarmonyPatch(typeof(MsgSys), nameof(MsgSys.Update))] // This one is the important guy for fixing the dialogue bug.
-    [HarmonyPatch(typeof(King), nameof(King.Update))]
     public static IEnumerable<CodeInstruction> Uncap(IEnumerable<CodeInstruction> instructions) {
-        foreach (var instr in instructions) {
-            if (instr.LoadsConstant()) {
-                if (instr.OperandIs(0.03333333f) || instr.OperandIs(0.01666667f)) {
-                    instr.operand = 1.0f / TARGET_FPS;
-                }
-            }
+        var deltaFrame = AccessTools.Field(typeof(GameManager), nameof(GameManager.deltaFrame));
+        var mSimulation = AccessTools.Field(typeof(GameManager), nameof(GameManager.mSimulation));
+        var thirtieth = 0.03333333f;
 
-            yield return instr;
-        }
+        return new CodeMatcher(instructions)
+            // 683: if this.deltaFrame >= 1/30 {} (outer)
+            .MatchForward(false, new(OpCodes.Ldarg_0),
+                          new(OpCodes.Ldfld, deltaFrame),
+                          new(OpCodes.Ldc_R4, thirtieth),
+                          new(OpCodes.Blt_Un),
+                          new(OpCodes.Ldc_I4_0)).AssertPos(683)
+            // New condition: this.deltaFrame > 0f
+            .Advance(2).SetOperandAndAdvance(0f)
+            .SetOpcodeAndAdvance(OpCodes.Ble_Un)
+            // 693: this.deltaFrame -= 1/30;
+            .MatchForward(false,
+                          new(OpCodes.Ldarg_0),
+                          new(OpCodes.Dup),
+                          new(OpCodes.Ldfld, deltaFrame),
+                          new(OpCodes.Ldc_R4, thirtieth),
+                          new(OpCodes.Sub),
+                          new(OpCodes.Stfld, deltaFrame)).AssertPos(693)
+            // New: this.deltaFrame -= deltaTime;
+            // Yes, this is a bit silly. I don't want to refactor the target code much more than this,
+            // and getting this patch to work properly already took a lot of trial and error.
+            .Advance(3).SetInstruction(new(OpCodes.Ldloc_0))
+            // 762: this.mSimulation.DoTick(1/30)
+            .MatchForward(true,
+                          new(OpCodes.Ldarg_0),
+                          new(OpCodes.Ldfld, mSimulation),
+                          new(OpCodes.Ldc_R4, thirtieth)).AssertPos(764)
+            // new argument: deltaTime
+            .SetInstruction(new(OpCodes.Ldloc_0))
+            // 812: if this.deltaFrame >= 1/30 {} (inner)
+            .RemoveMatching(new(OpCodes.Ldarg_0),
+                            new(OpCodes.Ldfld, deltaFrame),
+                            new(OpCodes.Ldc_R4, thirtieth)).AssertPos(812)
+            // For some reason, this is a backward jump, so we need to keep it and just make it unconditional.
+            .SetOpcodeAndAdvance(OpCodes.Br)
+            .Instructions();
+    }
+
+    private static readonly FieldInfo
+        s32Time = AccessTools.Field(typeof(GameManager), nameof(GameManager.s32Time)),
+        tutoTimer = AccessTools.Field(typeof(SI_TUTORIAL), nameof(SI_TUTORIAL.s32Timer)),
+        gWork = AccessTools.Field(typeof(GameManager), nameof(GameManager.gWork)),
+        siTutorial = AccessTools.Field(typeof(GlobalWork), nameof(GlobalWork.siTutorial));
+
+    // TODO: Improve this further; patch ALL the per-frame stuff.
+    // This will do for now by stopping the level-start dialogue from sometimes failing to show up and softlocking the game.
+    // Some stuff is fine to stay capped at 30 or 60, but anyhing called by sMain() or that touches the same timers
+    // will need to be updated accordingly.
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.gYm_GameStateInitGameClear))]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.gYm_GameStateInitGameOver))]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sDemo))]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sGameClear))]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sGameOver))]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sTutoTitle))]
+    public static IEnumerable<CodeInstruction> PatchTimerIncrements(IEnumerable<CodeInstruction> instructions) {
+        var getDeltaTime = AccessTools.PropertyGetter(typeof(Time), nameof(Time.deltaTime));
+
+        var loadsTimer = new CodeMatch(OpCodes.Ldfld) { operands = { s32Time, tutoTimer } };
+        var storesTimer = new CodeMatch(OpCodes.Stfld) { operands = { s32Time, tutoTimer } };
+
+        return new CodeMatcher(instructions)
+            // Match all code patterns that increment or decrement a timer by 1
+            .MatchForward(false,
+                          loadsTimer,
+                          new(OpCodes.Ldc_I4_1),
+                          new(ci => ci.opcode == OpCodes.Add || ci.opcode == OpCodes.Sub),
+                          storesTimer)
+            .Repeat(cm => cm
+                // Replace the 1 with (int)(Time.deltaTime * 1000f)
+                .Advance(1)
+                .RemoveInstruction()
+                .Insert(new(OpCodes.Call, getDeltaTime),
+                        new(OpCodes.Ldc_R4, 1000f),
+                        new(OpCodes.Mul),
+                        new(OpCodes.Conv_I4)))
+            .Start()
+            // Match all code patterns that assign a (nonzero) constant to a timer
+            .MatchForward(false,
+                          new(ci => ci.LoadsConstant() && ci.opcode != OpCodes.Ldc_I4_0),
+                          storesTimer)
+            .Repeat(cm => cm
+                // Multiply that constant by the milliseconds-per-tick ratio
+                .SetInstructionAndAdvance(new(OpCodes.Ldc_I4, cm.Instruction.ConstInt() * 1000 / 30)))
+            .Start()
+            // Match all code patterns that branch if s32Time is nonzero
+            .MatchForward(false,
+                          new(OpCodes.Ldfld, s32Time),
+                          new(OpCodes.Brtrue))
+            .Repeat(cm => cm
+                // Instead branch if s32time is GREATER than zero
+                .Advance(1)
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ldc_I4_0))
+                .SetOpcodeAndAdvance(OpCodes.Bgt))
+            .Instructions();
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sGameOver))]
+    public static IEnumerable<CodeInstruction> TimerFixup1(IEnumerable<CodeInstruction> instructions) {
+        return new CodeMatcher(instructions)
+            .MatchForward(true,
+                          new(OpCodes.Ldfld, s32Time),
+                          new(OpCodes.Ldc_I4_S, (sbyte)60))
+            .SetInstruction(new(OpCodes.Ldc_I4, 2000)) // 60 frames -> 2000 ms
+            .Instructions();
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.sTutoTitle))]
+    public static IEnumerable<CodeInstruction> TimerFixup2(IEnumerable<CodeInstruction> instructions, ILGenerator gen) {
+        var matcher = new CodeMatcher(instructions)
+            .MatchForward(false,
+                          new(OpCodes.Ldfld, tutoTimer),
+                          new(OpCodes.Ldc_I4_S, (sbyte)22),
+                          new(OpCodes.Bne_Un))
+            .Advance(1)
+            .RemoveInstruction();
+
+        // Old C#: if (s32Timer == 22 frames)
+        // New C#: if (s32Timer <= 733 ms && s32Timer > 667 ms)
+        var target = (Label)matcher.Operand;
+        var loc = gen.DeclareLocal(typeof(int));
+        matcher
+            .RemoveInstruction()
+            .Insert(new(OpCodes.Stloc, loc),
+                    new(OpCodes.Ldloc, loc),
+                    new(OpCodes.Ldc_I4, 733),
+                    new(OpCodes.Bgt_Un, target), // the condition fails if s32Timer > 733 ms
+                    new(OpCodes.Ldloc, loc),
+                    new(OpCodes.Ldc_I4, 667),
+                    new(OpCodes.Ble_Un, target)); // the condition fails if s32Timer <= 667 ms
+
+        matcher
+            .MatchForward(false,
+                          new(OpCodes.Ldfld, tutoTimer),
+                          new(OpCodes.Ldc_I4_S, (sbyte)20),
+                          new(OpCodes.Bne_Un))
+            .Advance(1)
+            .RemoveInstruction();
+
+        target = (Label)matcher.Operand;
+        matcher
+            .RemoveInstruction()
+            .Insert(new(OpCodes.Ldc_I4, 667),
+                    new(OpCodes.Bgt_Un, target), // the condition fails if s32Timer > 667 ms (possibly superfluous but I'd rather be safe)
+                    new(OpCodes.Ldloc, loc), // We already stored it in our personal local variable in the last bit.
+                    new(OpCodes.Ldc_I4_0),
+                    new(OpCodes.Ble_Un, target)); // the condition fails if s32Timer <= 0
+
+        return matcher.Instructions();
     }
 
     [HarmonyTranspiler]
