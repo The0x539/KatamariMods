dc602f7bb4422f5b5d7a3860a55858338c030a14
The0x539 <the0x539@gmail.com>
2026-01-29 17:42:35 -0600

    Get DoF in a pretty okay state

--- DevUtils/DepthOfField.cs
+++ DevUtils/DepthOfField.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 
+using System;
+
 using UnityEngine;
 
 using UnityEngine.PostProcessing;
@@ -7,16 +9,23 @@ using UnityEngine.PostProcessing;
 namespace DevUtils;
 
 static class DepthOfField {
+    // TODO: This probably isn't the place to put the updater component on the camera.
+    // I need to learn more about the "life cycles" of some of these objects and components.
     [HarmonyPostfix]
     [HarmonyPatch(typeof(GlobalManager), nameof(GlobalManager.SetActiveStageObject))]
-    public static void EnableDof2() {
+    public static void EnableDof() {
         var gw = GlobalWork.Instance;
         if (gw.u8GameMode != DefineEnum.GI_GMODE.GI_GMODE_ENDING) {
-            foreach (var cam in gw.camGame) {
+            for (var i = 0; i <= 1; i++) {
+                var cam = gw.camGame[i];
                 if (cam == null) continue;
 
                 var ppb = cam.GetComponent<PostProcessingBehaviour>();
                 ppb.profile.depthOfField.enabled = QualitySetting.Instance.IsDOF;
+                // This should probably be a graphics option
+                ppb.profile.depthOfField.settings = ppb.profile.depthOfField.settings with { kernelSize = DepthOfFieldModel.KernelSize.VeryLarge };
+
+                cam.gameObject.AddComponent<UpdateDof>();
             }
         }
     }
@@ -26,34 +35,31 @@ static class DepthOfField {
 
     [HarmonyPrefix]
     [HarmonyPatch(typeof(DepthOfFieldComponent), nameof(DepthOfFieldComponent.Prepare))]
-    public static void OnDof(DepthOfFieldComponent __instance) {
+    public static void DepthPassToFixDof(DepthOfFieldComponent __instance) {
         var ctx = __instance.context;
 
         if (depthCamera == null) {
             depthCamera = new GameObject().AddComponent<Camera>();
-            var desc = ctx.camera.targetTexture.descriptor;
+            depthCamera.enabled = false;
 
-            desc.colorFormat = RenderTextureFormat.R8;
-            desc.depthBufferBits = 0;
+            // This msaaSamples thing was very difficult to hunt down and made the whole thing not work.
+            var desc = ctx.camera.targetTexture.descriptor with { msaaSamples = 1 };
+
+            desc = desc with { colorFormat = RenderTextureFormat.R8, depthBufferBits = 0 };
             depthCamColor = new RenderTexture(desc) { name = "Depth Pass - Color" };
 
-            desc.colorFormat = RenderTextureFormat.Depth;
-            desc.depthBufferBits = 16;
+            desc = desc with { colorFormat = RenderTextureFormat.Depth, depthBufferBits = 16 };
             depthCamDepth = new RenderTexture(desc) { name = "Depth Pass - Depth" };
-
-            depthCamera.SetTargetBuffers(depthCamColor.colorBuffer, depthCamDepth.depthBuffer);
         }
 
-        var depthCam = depthCamera.GetComponent<Camera>();
-        depthCam.enabled = false;
-
-        var blitToDepth = ctx.materialFactory.Get("Hidden/BlitToDepth");
+        var depthCam = depthCamera;
 
         depthCam.CopyFrom(ctx.camera);
         depthCam.SetTargetBuffers(depthCamColor.colorBuffer, depthCamDepth.depthBuffer);
         depthCam.Render();
 
         RenderTexture.active = Shader.GetGlobalTexture("_CameraDepthTexture") as RenderTexture;
+        var blitToDepth = ctx.materialFactory.Get("Hidden/BlitToDepth");
         blitToDepth.SetPass(0);
         blitToDepth.SetTexture("_MainTex", depthCamDepth);
         DrawQuad();
@@ -77,5 +83,41 @@ static class DepthOfField {
         GL.Vertex3(0, 1, 0);
 
         GL.End();
+    }
+}
+
+// A little object/component with two purposes:
+//   - Keep the focal plane "lined up" with the katamari at all times
+//   - Make it easier to tweak the DoF parameters, since UnityExplorer only has sliders in its UI for transforms.
+public sealed class UpdateDof : MonoBehaviour {
+    public DepthOfFieldModel dofModel = null!;
+    public Transform katamari = null!;
+    public Transform adjuster = null!;
+
+    public void Start() {
+        var gw = GlobalWork.instance;
+        var i = Array.IndexOf(gw.camGame, this.GetComponent<Camera>());
+        this.katamari = gw.player[i].KatamariTransform;
+
+        this.dofModel = this.GetComponent<PostProcessingBehaviour>().profile.depthOfField;
+
+        this.adjuster = new GameObject("DoF Adjust").transform;
+        this.adjuster.parent = this.transform;
+
+        var s = this.dofModel.settings;
+        // increase the focal length for now, to make it more obvious for testing
+        // (the chosen factor is completely arbitrary based on what looks good in MAS 4 and Eternal 3)
+        // definitely not final: this is way too strong in Eternal 1 and probably other Takeda Residence levels
+        this.adjuster.localScale = new(s.aperture, s.focalLength * 2.6f, 1f);
+    }
+
+    // First person mode doesn't play very well with this - perhaps use the "desired follow distance" in such cases?
+    public void Update() {
+        var s = this.adjuster.localScale;
+        this.dofModel.settings = this.dofModel.settings with {
+            aperture = s.x,
+            focalLength = s.y,
+            focusDistance = s.z * Vector3.Distance(this.transform.position, this.katamari.position),
+        };
     }
 }
