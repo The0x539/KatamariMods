e509377095041cf4555a8e3cdd0aebda7c413f10
The0x539 <the0x539@gmail.com>
2026-02-11 17:45:51 -0600

    Add entries to the options menu

--- DevUtils/OptionsMenu.cs
+++ DevUtils/OptionsMenu.cs
@@ -6,6 +6,8 @@ using MyGame;
 using MyGame.InputStatus;
 
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection.Emit;
 
 using UnityEngine;
@@ -232,6 +234,124 @@ static class OptionsMenu {
             .MatchForward(false, [new(OpCodes.Brtrue)])
             .SetOpcodeAndAdvance(OpCodes.Brfalse)
             .Instructions();
+    }
+
+    private static readonly string QualitySettingsPath = Path.Combine(FileManager.SaveTemporaryPath, "Setting/quality.ex.setting");
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(QualitySetting), nameof(QualitySetting.Load))]
+    public static bool LoadExtraSettings(QualitySetting __instance) {
+        if (!File.Exists(QualitySettingsPath)) return true;
+
+        try {
+            var status = File.ReadAllText(QualitySettingsPath)
+                .Split(',')
+                .Select(int.Parse)
+                .ToList();
+
+            while (status.Count < __instance.ItemMax) status.Add(0);
+
+            if (__instance.statusNo[0] >= __instance.itemName.Count) __instance.statusNo[0] = 0;
+            if (__instance.statusNo[4] >= QualitySetting.antialiasingValue.Length) __instance.statusNo[4] = 1;
+
+            return false;
+        } catch {
+            return true;
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(QualitySetting), nameof(QualitySetting.Load))]
+    public static void PadSettings(QualitySetting __instance) {
+        while (__instance.statusNo.Length < __instance.ItemMax) {
+            __instance.statusNo = [.. __instance.statusNo, 0];
+        }
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(QualitySetting), nameof(QualitySetting.Save))]
+    public static bool SaveExtraSettings(QualitySetting __instance) {
+        var path = Path.Combine(FileManager.SaveTemporaryPath, "Setting/quality.ex.setting");
+        var text = __instance.statusNo.Join(n => n.ToString(), ",");
+        File.WriteAllText(path, text);
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(QualitySetting), MethodType.Constructor)]
+    public static void CreateExtraSettings(QualitySetting __instance) {
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(QualitySetting), MethodType.Constructor)]
+    public static void ExposeExtraSettings(QualitySetting __instance) {
+        var items = __instance.itemName;
+        items.Add(["16x", "1x", "2x", "4x", "8x"]);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UguiUtility), nameof(UguiUtility.Awake))]
+    public static void ExtendSettingsUI(UguiUtility __instance) {
+        if (__instance.gameObject.scene.name != "Quality") return;
+
+        Transform get(string name) => __instance.transform.Find(name);
+
+        var labelPre = get("TextItem7");
+        var valuePre = get("TextItemExp7");
+        var leftPre = get("Button7L");
+        var rightPre = get("Button7R");
+
+        // Text item 6 comes after 7 in the vanilla layout, and it's the disabled DoF option
+        // TODO: Consider just instantiating my own setting for the DoF? The vanilla one, being disabled, is a little inconsistent
+        // TODO: Off/Low/Med/High DoF options
+        // TODO: Actually honor the DoF and AF settings rather than just giving them a UI
+
+        get("TextItem6").gameObject.SetActive(true);
+        get("TextItemExp6").gameObject.SetActive(true);
+        get("Button6L").gameObject.SetActive(true);
+        get("Button6R").gameObject.SetActive(true);
+
+        var spacing = get("TextItem1").position.y - get("TextItem0").position.y;
+
+        var prevFocus = get("TextItem6");
+        LinkFocus(get("TextItem7"), prevFocus);
+
+        for (var i = 8; i < QualitySetting.Instance.ItemMax; i++) {
+            var offset = new Vector2(0, spacing * (i - 6));
+
+            var label = Object.Instantiate(labelPre, labelPre.parent); // TODO: I didn't know this overload exists and should use it in more places.
+            label.name = $"TextItem{i}";
+            label.transform.Translate(offset);
+            var value = Object.Instantiate(valuePre, valuePre.parent);
+            value.name = $"TextItemExp{i}";
+            value.transform.Translate(offset);
+
+            var left = Object.Instantiate(leftPre, leftPre.parent);
+            left.name = $"Button{i}L";
+            left.transform.Translate(offset);
+
+            var right = Object.Instantiate(rightPre, rightPre.parent);
+            right.name = $"Button{i}R";
+            right.transform.Translate(offset);
+
+            LinkFocus(prevFocus, label);
+            prevFocus = label;
+            label.GetComponent<UguiFocus>().focusID = i;
+
+            label.GetComponent<UITextLocalizer>().textID = i switch {
+                8 => "UI_SYS_213",
+                _ => "UI_TTR_022", // Placeholder
+            };
+        }
+
+        LinkFocus(prevFocus, get("TextItem0"));
+    }
+
+    private static void LinkFocus(Component prev, Component next) {
+        var prevF = prev.GetComponent<UguiFocus>();
+        var nextF = next.GetComponent<UguiFocus>();
+        prevF.downKeyMove = nextF;
+        nextF.upKeyMove = prevF;
     }
 
     // TODO: Update quality settings mid-level, e.g. ambient occlusion
