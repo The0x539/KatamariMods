69bb1351d04021c1ff52999b59e6b8913848c1ee
The0x539 <the0x539@gmail.com>
2026-02-13 23:50:04 -0600

    Move MSAA-everywhere to graphics mod

--- GraphicalEnhancements/Plugin.cs
+++ GraphicalEnhancements/Plugin.cs
@@ -2,15 +2,37 @@
 
 using HarmonyLib;
 
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using UnityEngine;
+
 namespace GraphicalEnhancements;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public sealed class Plugin : BaseUnityPlugin {
     public void Awake() {
         IngameOptions.AddListener();
+        Harmony.CreateAndPatchAll(typeof(Plugin));
         Harmony.CreateAndPatchAll(typeof(IngameOptions));
         Harmony.CreateAndPatchAll(typeof(ExtraOptions));
         Harmony.CreateAndPatchAll(typeof(DepthOfField));
         Harmony.CreateAndPatchAll(typeof(HighResRenderTargets));
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(CameraKatamari), nameof(CameraKatamari.Setup))]
+    [HarmonyPatch(typeof(MonoOnlyCamera), nameof(MonoOnlyCamera.SetTargetTexture))]
+    [HarmonyPatch(typeof(UIFixedCamera), nameof(UIFixedCamera.Start))]
+    [HarmonyPatch(typeof(UIMonoCamera), nameof(UIMonoCamera.SetTargetTextureDonotAddCamera))]
+    public static IEnumerable<CodeInstruction> MsaaEverywhere(IEnumerable<CodeInstruction> instructions) {
+        var setAllowMSAA = AccessTools.PropertySetter(typeof(Camera), nameof(Camera.allowMSAA));
+
+        return new CodeMatcher(instructions)
+            .MatchForward(false,
+                          new(OpCodes.Ldc_I4_0),
+                          new(OpCodes.Callvirt, setAllowMSAA))
+            .SetOpcodeAndAdvance(OpCodes.Ldc_I4_1)
+            .Instructions();
     }
 }
