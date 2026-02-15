51e1be5e4cee0c0489ad8eaf7783d206a7b0e439
The0x539 <the0x539@gmail.com>
2026-02-15 04:47:34 -0600

    Create prototype for loading custom player models

--- SingleplayerCousins/Extensions.cs
+++ SingleplayerCousins/Extensions.cs
@@ -28,4 +28,8 @@ internal static class Extensions {
         }
         return list;
     }
+
+    public static Vector3 ToUnity(this Assimp.Vector3D v) => new(v.X, v.Y, v.Z);
+    public static Vector2 ToUnityVec2(this Assimp.Vector3D v) => new(v.X, v.Y);
+    public static Matrix4x4 ToUnity(this Assimp.Matrix4x4 v) => new(new(v.A1, v.B1, v.C1, v.D1), new(v.A2, v.B2, v.C2, v.D2), new(v.A3, v.B3, v.C3, v.D3), new(v.A4, v.B4, v.C4, v.D4));
 }
