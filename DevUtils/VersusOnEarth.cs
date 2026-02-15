3bdaeca56067802fcf8b898bb3d2bd55ed6e2dc3
The0x539 <the0x539@gmail.com>
2026-02-15 13:05:38 -0600

    Create a prototype for versus mode on Earth

---
+++ DevUtils/VersusOnEarth.cs
@@ -0,0 +1,32 @@
+﻿using DefineEnum;
+
+using HarmonyLib;
+
+namespace DevUtils;
+
+public static class VersusOnEarth {
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GlobalWork), nameof(GlobalWork.SetRandomMission))]
+    public static void Foo(GlobalWork __instance) {
+        __instance.playMission = GAMEINFO_MIS.GAMEINFO_MIS_03;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GlobalWork), nameof(GlobalWork.InitObjMessage))]
+    public static void Bar(GlobalWork __instance) {
+        __instance.playMission = GAMEINFO_MIS.GAMEINFO_VS_00;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GlobalWork), nameof(GlobalWork.LoadMissionData))]
+    public static void Baz(GlobalWork __instance) {
+        __instance.playMission = GAMEINFO_MIS.GAMEINFO_MIS_03;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SimulationNativeMethods), nameof(SimulationNativeMethods.Init))]
+    [HarmonyPatch(typeof(SimulationNativeMethods), nameof(SimulationNativeMethods.MonoInitStart))]
+    public static void Qux(ref int mission) {
+        mission = (int)GAMEINFO_MIS.GAMEINFO_VS_00;
+    }
+}
