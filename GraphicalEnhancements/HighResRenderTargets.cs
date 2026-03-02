79e603a24fc884af175998020e739982a84a43d3
The0x539 <the0x539@gmail.com>
2026-03-02 00:41:08 -0600

    Supersample the katamari result image

--- GraphicalEnhancements/HighResRenderTargets.cs
+++ GraphicalEnhancements/HighResRenderTargets.cs
@@ -16,22 +16,33 @@ static class HighResRenderTargets {
         var getWidth = AccessTools.Method(typeof(HighResRenderTargets), nameof(GetWidth));
         var getHeight = AccessTools.Method(typeof(HighResRenderTargets), nameof(GetHeight));
 
+        // We need to supersample it a bit because MSAA only applies to face edges, not along sharp lines in textures.
+        // This game's art style has... a lot of sharp lines in textures.
         return new CodeMatcher(instructions, generator)
             .MatchForward(false,
                           new(OpCodes.Ldc_R4, 1920f),
                           new(OpCodes.Ldc_R4, 1080f))
             .Repeat(cm => cm
                 .SetAndAdvance(OpCodes.Call, getWidth)
-                .InsertAndAdvance([new(OpCodes.Conv_R4)])
+                .InsertAndAdvance(new(OpCodes.Dup),
+                                  new(OpCodes.Add),
+                                  new(OpCodes.Conv_R4))
                 .SetAndAdvance(OpCodes.Call, getHeight)
-                .InsertAndAdvance([new(OpCodes.Conv_R4)]))
+                .InsertAndAdvance(new(OpCodes.Dup),
+                                  new(OpCodes.Add),
+                                  new(OpCodes.Conv_R4)))
             .Start()
             .MatchForward(false,
                           new(OpCodes.Ldc_I4, 1920),
                           new(OpCodes.Ldc_I4, 1080))
             .Repeat(cm => cm
                 .SetAndAdvance(OpCodes.Call, getWidth)
-                .SetAndAdvance(OpCodes.Call, getHeight))
+                .InsertAndAdvance(new(OpCodes.Dup),
+                                  new(OpCodes.Add))
+                .SetAndAdvance(OpCodes.Call, getHeight)
+                .InsertAndAdvance(new(OpCodes.Dup),
+                                  new(OpCodes.Add))
+)
             .Instructions();
     }
 
@@ -40,7 +51,7 @@ static class HighResRenderTargets {
     public static void AntialiasKatamariResultImage(CameraKatamari __instance) {
         var descriptor = CameraKatamari._rTexture.descriptor;
         CameraKatamari._rTexture.Release();
-        descriptor.msaaSamples = 8;
+        descriptor.msaaSamples = 4;
         var rt = new RenderTexture(descriptor);
         __instance._camera.targetTexture = CameraKatamari._rTexture = rt;
         if (__instance._rImage != null) __instance._rImage.texture = rt;
