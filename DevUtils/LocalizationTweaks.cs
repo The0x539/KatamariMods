b515b927f0543d096b0bb99fa15f483881c48439
The0x539 <the0x539@gmail.com>
2026-03-15 17:11:11 -0500

    Rename Colombo -> Njamo

--- DevUtils/LocalizationTweaks.cs
+++ DevUtils/LocalizationTweaks.cs
@@ -16,9 +16,22 @@ public static class LocalizationTweaks {
         {"KG_O_RSL_319", "Do you want to make this Polaris?{^5}\nOr stardust?" },
     };
 
+    private static readonly HashSet<string> colombo = [
+        "KG_C_URS_010",
+        "UI_MSH_040",
+        "OT_OBJ_1495",
+        "OT_OBJ_1713",
+        "OT_KNG_021",
+    ];
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(Entity_king_text), nameof(Entity_king_text.GetLocaliseText))]
     public static void OverrideLocalization(string id, int local, ref string __result) {
+        if (colombo.Contains(id)) {
+            __result = __result.Replace("Colombo", "Njamo").Replace("コロンボ", "ンジャモ");
+            return;
+        }
+
         if (local != (int)LANGUAGE.ENGLISH) return;
 
         if (replacements.TryGetValue(id, out var text)) {
