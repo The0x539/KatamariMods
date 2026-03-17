76e712e2fcd8c8513b2e188b6193e685e411d3d5
The0x539 <the0x539@gmail.com>
2026-03-16 23:17:46 -0500

    Procedral animation for Dega's tail?!

--- SingleplayerCousins/Cousins/Dega.cs
+++ SingleplayerCousins/Cousins/Dega.cs
@@ -47,11 +47,13 @@ public sealed class Dega : MonoBehaviour {
             var hover_dy = 0.1f * Mathf.Sin(6.13f * this.theta) * dt;
             this.root.localPosition += new Vector3(0, hover_dy, 0);
         } else {
-            // TODO: Ever since preparing the bindposes or whatever, this is all messed up.
-            float y = 1.2f * Mathf.Sin(this.theta * 0.431f + 1.365f), // Frequency and phase chosen arbitrarily to stop the axes from harmonizing
-                  z = 50 * Mathf.Sin(this.theta) - 35,                // The tail on the model naturally faces ~30-35 degrees to the left.
-                  x = 5 * Mathf.Sin(this.theta * 0.789f + 2.718f);
-            this.tail[0].localRotation = this.tailRestPose[0] * Quaternion.Euler(x, y, z);
+            var baseTwist = 23.21f * Mathf.Sin(this.theta * 1.56f) - 22.3f;
+            this.tail[0].localRotation = this.tailRestPose[0] * Quaternion.Euler(20f, 20f, baseTwist);
+
+            for (var i = 1; i < this.tail.Length; i++) {
+                var twist = Mathf.Sin(this.theta * 1.27f - (i * 0.87f)) * 19f * Mathf.Sqrt(i);
+                this.tail[i].localRotation = this.tailRestPose[i] * Quaternion.Euler(0, 0, twist);
+            }
         }
     }
 }
