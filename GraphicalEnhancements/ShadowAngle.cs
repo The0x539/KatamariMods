215631ef0253d942c603390a1ff4cf119aa1e940
The0x539 <the0x539@gmail.com>
2026-03-17 13:17:05 -0500

    Fix small bug in the ShadowAngle patch

--- GraphicalEnhancements/ShadowAngle.cs
+++ GraphicalEnhancements/ShadowAngle.cs
@@ -11,13 +11,16 @@ static class ShadowAngle {
     [HarmonyPatch(typeof(Player), nameof(Player.Update))]
     public static void UpdateShadow(Player __instance) {
         var shadow = __instance.ShadowTransform;
-        var ray = new Ray(shadow.position, Vector3.down);
 
-        Quaternion target;
-        if (Physics.Raycast(ray, out var hit, 100f)) {
+        // The shadow is right on the ground, so we need to start *slightly* above the ground.
+        // Otherwise, most of the time, the raycast will just fail, and the angle will default to horizontal.
+        // However, if a sloped surface extends past the ground to under it, the raycast would hit that instead.
+        // A good example of this is directly behind the starting location in Make A Star 7.
+        var ray = new Ray(shadow.position + Vector3.up, Vector3.down);
+
+        var target = Quaternion.identity;
+        if (Physics.Raycast(ray, out var hit)) {
             target = Quaternion.FromToRotation(Vector3.up, hit.normal);
-        } else {
-            target = Quaternion.identity;
         }
 
         // I'm not too worried about this being *properly* delta-timed; it just needs to be vaguely "smooth"./
