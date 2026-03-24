fd391bada0ba403ac0a239823787eab048b2b76a
The0x539 <the0x539@gmail.com>
2026-03-24 14:41:12 -0500

    Add anisotropic filtering to the end credits

--- GraphicalEnhancements/Plugin.cs
+++ GraphicalEnhancements/Plugin.cs
@@ -88,6 +88,17 @@ public sealed class Plugin : BaseUnityPlugin {
     [HarmonyPatch(typeof(KinokoRotator), nameof(KinokoRotator.Start))]
     public static void AnisotropicKinoko(KinokoRotator __instance) => MakeAnisotropic(__instance._pre_kinoko.transform.Find("pre_kinoko"));
 
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(EndingController), nameof(EndingController.Start))]
+    public static void AnisotropicCredits(EndingController __instance) {
+        MakeAnisotropic(__instance._animator_kinoko);
+        MakeAnisotropic(__instance._animator_oujiStar);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(EndingEarthController), nameof(EndingEarthController.Start))]
+    public static void AnisotropicCredits(EndingEarthController __instance) => MakeAnisotropic(__instance);
+
     private static void MakeAnisotropic(Component t) => MakeAnisotropic(t.gameObject);
 
     private static void MakeAnisotropic(GameObject obj) {
