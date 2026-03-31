75b12663382efe6f6b4a0c19cc2181a4e6caf9c2
The0x539 <the0x539@gmail.com>
2026-03-30 22:22:00 -0500

    Actually update mesh material when doing distance fade

--- GraphicalEnhancements/TransparencyFixes.cs
+++ GraphicalEnhancements/TransparencyFixes.cs
@@ -39,6 +39,8 @@ public static class TransparencyFixes {
                 self.isChangeMaterial = true;
             }
         }
+
+        self.SetMeshMaterial(self.activeScene, self.mRenderers, self.smRenderers);
     }
 
     [HarmonyPrefix]
